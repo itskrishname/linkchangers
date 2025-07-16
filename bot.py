cbb7bb9f16c0bd00da28e820ef2d93816f038e26
Krishna <kd259457@gmail.com>
2025-07-16 18:27:27 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, PORT
 from plugins import web_server
 import pyrogram.utils
 from aiohttp import web
+
 pyrogram.utils.MIN_CHANNEL_ID = -1009147483647
 
 name = """
@@ -26,20 +27,20 @@ class Bot(Client):
         self.LOGGER = LOGGER
 
     async def start(self, *args, **kwargs):
-    await super().start(*args, **kwargs)
-    usr_bot_me = await self.get_me()
-    self.uptime = datetime.now()
+        await super().start(*args, **kwargs)
+        usr_bot_me = await self.get_me()
+        self.uptime = datetime.now()
 
-    self.set_parse_mode(ParseMode.HTML)
-    self.LOGGER(__name__).info("Bot Running..!\n\nCreated by \nhttps://t.me/Okabe_xRintarou")
-    self.LOGGER(__name__).info(f"{name}")
-    self.username = usr_bot_me.username
+        self.set_parse_mode(ParseMode.HTML)
+        self.LOGGER(__name__).info("Bot Running..!\n\nCreated by \nhttps://t.me/Okabe_xRintarou")
+        self.LOGGER(__name__).info(f"{name}")
+        self.username = usr_bot_me.username
 
-    # Web-response
-    app = web.AppRunner(await web_server())
-    await app.setup()
-    bind_address = "0.0.0.0"
-    await web.TCPSite(app, bind_address, PORT).start()
+        # Web-response
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, PORT).start()
 
     async def stop(self, *args):
         await super().stop()
