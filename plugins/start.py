97ec744c1f5936bf0e351e8e0b74f1ef03082a85
Krishna <kd259457@gmail.com>
2025-07-16 18:35:39 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -156,7 +156,7 @@ async def help_callback(client: Bot, callback_query):
     
     
     await callback_query.answer()
-    await callback_query.message.edit_text("<b><i>About Us..\n\n‣ Made for : @Anime_X_Hunters\n‣ Owned by : @Okabe_xRintarou\n‣ Developer : @The_Seishiro_Nagi\n\n Adios !!</i></b>", reply_markup=inline_buttons)
+    await callback_query.message.edit_text("<b><i>About Us..\n\n‣ Made for : @SECRECT_BOT_UPDATES\n‣ Owned by : @NEW_ANIMES_HINDI_DUB_INDIA\n‣ Developer : @in_hindi_dub_korean_drama\n\n Adios !!</i></b>", reply_markup=inline_buttons)
 
 @Bot.on_callback_query(filters.regex("close"))
 async def close_callback(client: Bot, callback_query):
