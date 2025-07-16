a3a749ee31fea25d8ab61d02a89e45ca4bd624fe
Krishna <kd259457@gmail.com>
2025-07-16 17:56:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,29 +5,29 @@ from logging.handlers import RotatingFileHandler
 
 #Recommended
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "0")
-APP_ID = int(os.environ.get("APP_ID", "0"))
-API_HASH = os.environ.get("API_HASH", "0")
+APP_ID = int(os.environ.get("APP_ID", "24828197"))
+API_HASH = os.environ.get("API_HASH", "d36e278e89ebeb900aeda4128d413a77")
 
 ##---------------------------------------------------------------------------------------------------
 
 #Main 
-OWNER_ID = int(os.environ.get("OWNER_ID", "5090651635"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "7660990923"))
 PORT = os.environ.get("PORT", "8010")
 
 ##---------------------------------------------------------------------------------------------------
 
 #Database
-DB_URI = os.environ.get("DATABASE_URL", "0")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://musicbhaikon9910:krishna@cluster0.cwvegmt.mongodb.net/")
 DB_NAME = os.environ.get("DATABASE_NAME", "links-sharing")
 
 ##---------------------------------------------------------------------------------------------------
 
 #Default
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
-START_MSG = os.environ.get("START_MESSAGE", "") #No Need keep it blank
+START_MSG = os.environ.get("START_MESSAGE", "hii hello users") #No Need keep it blank
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "5090651635").split()):
+    for x in (os.environ.get("ADMINS", "7660990923").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
@@ -42,7 +42,7 @@ USER_REPLY_TEXT = None
 
 #Admin == OWNERID
 ADMINS.append(OWNER_ID)
-ADMINS.append(5090651635)
+ADMINS.append(7660990923)
 
 ##---------------------------------------------------------------------------------------------------
 
